e53826b9066f60a7e44ea64cb8db713540f9c089
robin-yoo <robinyoo25@gmail.com>
2017-06-30 14:49:15 -0700

    Update media.py

--- media.py
+++ media.py
@@ -5,26 +5,26 @@ Module to display movie object, attributes, and instances
 
 import webbrowser
 
+
 # The movie blueprint
 class Movie():
     """
     Class object stores movie related information
     """
-    
+
     # Create 4 properties or parameters for the movie
     def __init__(self, title, storyline, poster_image, trailer_youtube):
         """
         Initialize instances of class Movie
-        
+
         :param title: title
         :param storyline: storyline
         :param poster_image_url: poster_image_url
         :param trailer_youtube_url: trailer_youtube
         """
-        
+
         self.title = title
         self.storyine = storyline
-        self.poster_image_url = poster_image
         self.trailer_youtube_url = trailer_youtube
 
     # Open the youtube trailer in a browser
@@ -34,6 +34,5 @@ class Movie():
 
         :return: webbrowser to play trailer
         """
-        
+
         webbrowser.open(self.trailer_youtube_url)
-        
